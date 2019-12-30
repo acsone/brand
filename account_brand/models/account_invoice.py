a7a530822e293f867964eaa8fa18ae68ecafe1c1
sbejaoui <souheil.bejaoui@acsone.eu>
2019-12-30 14:55:02 +0100

    [IMP] - set the invoice line analytic account at brand change

--- account_brand/models/account_invoice.py
+++ account_brand/models/account_invoice.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -16,3 +16,14 @@ class AccountInvoice(models.Model):
             'cancel': [('readonly', True)],
         }
     )
+
+    @api.onchange('brand_id', 'invoice_line_ids')
+    def _onchange_brand_id(self):
+        res = super()._onchange_brand_id()
+        for invoice in self:
+            if invoice.state == 'draft' and invoice.brand_id:
+                account_analytic = invoice.brand_id.analytic_account_id
+                invoice.invoice_line_ids.update(
+                    {'account_analytic_id': account_analytic.id}
+                )
+        return res
