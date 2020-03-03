2a8097337b07657aa51c58baee307ce29204e3cf
sbejaoui <souheil.bejaoui@acsone.eu>
2020-03-03 15:57:59 +0100

    [IMP] - Use a an abstract model for onchange_brand_allowed_payment_mode

--- contract_payment_mode_brand/models/contract_contract.py
+++ contract_payment_mode_brand/models/contract_contract.py
@@ -6,15 +6,8 @@ from odoo import api, models
 
 class ContractContract(models.Model):
 
-    _inherit = "contract.contract"
-
-    @api.onchange("brand_id")
-    def onchange_brand_allowed_payment_mode(self):
-        self.ensure_one()
-        domain = []
-        if self.brand_id and self.brand_id.allowed_payment_mode_ids:
-            domain = [("id", "in", self.brand_id.allowed_payment_mode_ids.ids)]
-        return {"domain": {"payment_mode_id": domain}}
+    _name = "contract.contract"
+    _inherit = ["contract.contract", "res.brand.allowed.payment.mode.mixin"]
 
     @api.onchange("partner_id")
     def on_change_partner_id(self):
