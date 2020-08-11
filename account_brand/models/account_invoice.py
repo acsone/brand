6bfb4d642845a579b1fd527925b0f06331150e17
Quentin Groulard <quentin.groulard@acsone.eu>
2020-08-11 17:19:21 +0200

    [12.0][FIX] Create credit note with brand

--- account_brand/models/account_invoice.py
+++ account_brand/models/account_invoice.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -16,3 +16,13 @@ class AccountInvoice(models.Model):
             'cancel': [('readonly', True)],
         }
     )
+
+    @api.model
+    def _prepare_refund(self, invoice, date_invoice=None, date=None,
+                        description=None, journal_id=None):
+        values = super(AccountInvoice, self)._prepare_refund(
+            invoice, date_invoice=date_invoice, date=date,
+            description=description, journal_id=journal_id)
+        if invoice.brand_id:
+            values['brand_id'] = invoice.brand_id.id
+        return values
